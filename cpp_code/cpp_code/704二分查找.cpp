180317e8f0f2eb021e28284859f26832b7fe45d8
yuanshun-guo <guo_13797319692@163.com>
2022-11-29 12:56:53 +0800

    二分查找，左闭右开，中值对比，范围缩小

--- cpp_code/cpp_code/704二分查找.cpp
+++ cpp_code/cpp_code/704二分查找.cpp
@@ -1,41 +1,45 @@
-//#include <iostream>
-//#include <vector>
-//using namespace std;
-//
-//class Solution
-//{
-//public:
-//	int search(vector<int>& nums, int target)
-//	{
-//		int left = 0;
-//		int right = nums.size();
-//		while (left < right)
-//		{
-//			//int middle = left + (right - right) >> 1;
-//			int middle = left + (right - right) / 2;
-//			if (nums[middle] > target)
-//			{
-//				right = middle;
-//			}
-//			else if (nums[middle] < target)
-//			{
-//				left = middle + 1;
-//			}
-//			else
-//			{
-//				return middle;
-//			}
-//		}
-//		return -1;
-//	}
-//};
-//
-//
-//int main()
-//{
-//
-//	system("pause");
-//
-//	return 0;
-//
-//}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution 
+{
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int size_nums = nums.size();
+        if ((target > nums[size_nums - 1]) || (target < nums[0]))
+        {
+            return -1;
+        }
+        int right = size_nums;
+        int left = 0;
+        while(left < right)   //选择左闭右开[left, right) ，也就是说取值范围在left-->right-1，因此left不可能等于right
+        {
+            int mid = (right - left) / 2 + left;  //请注意我们是用中值进行的对比
+            if (target < nums[mid])
+            {
+                right = mid; //由于选择的是左闭右开，所以接下来是[left, mid)
+            }
+            else if (target > nums[mid])
+            {
+                left = mid + 1; //接下来是[mid + 1, right)
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
+
+
+int main()
+{
+
+	system("pause");
+
+	return 0;
+
+}
