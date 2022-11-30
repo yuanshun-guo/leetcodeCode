d9a577a04a0ca362430c92b20de2726da59e4795
yuanshun-guo <guo_13797319692@163.com>
2022-11-30 22:02:50 +0800

    新建vector+左右指针

--- cpp_code/cpp_code/977有序数组的平方.cpp
+++ cpp_code/cpp_code/977有序数组的平方.cpp
@@ -1,35 +1,34 @@
-//#include <iostream>
-//#include <vector>
-//using namespace std;
-//
-//class Solution
-//{
-//public:
-//	vector<int> sortedSquares(vector<int>& nums)
-//	{
-//		int k = nums.size() - 1; //数组最后一个元素的下标
-//		vector<int> result(nums.size(), 0);  //初始化，类似resize()
-//		for (int i = 0, j = nums.size() - 1; i <= j; )
-//		{
-//			if (nums[i] * nums[i] < nums[j] * nums[j])
-//			{
-//				result[k--] = nums[j] * nums[j];
-//				j--;
-//			}
-//			else
-//			{
-//				result[k--] = nums[i] * nums[i];
-//				i++;
-//			}
-//		}
-//		return result;
-//	}
-//};
-//
-//
-//int main()
-//{
-//	system("pause");
-//
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+        int k = nums.size();
+        vector<int> result(k, 0); //k个0
+        for (int i = 0, j = nums.size() - 1; i <= j; )
+        {
+            if (nums[i] * nums[i] < nums[j] * nums[j])
+            {
+                result[--k] = nums[j] * nums[j];
+                j--;
+            }
+            else
+            {
+                result[--k] = nums[i] * nums[i];
+                i++;
+            }
+        }
+        return result;
+    }
+};
+
+
+int main()
+{
+	system("pause");
+
+	return 0;
+}
